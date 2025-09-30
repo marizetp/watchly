4f0b6d503a219e44a15008ee98568345e77db602
Marizet Pedro <marizet.contact@gmail.com>
2025-09-30 16:38:50 +0100

    creating the star component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+//import './index.css';
+//import App from './App';
+import Star from './Star';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Star maxRating={20}></Star>
+    <Star></Star>
   </React.StrictMode>
 );
 
