2e369c6c2ad1c24716c5d9b6904f6a32db932458
Marizet Pedro <marizet.contact@gmail.com>
2025-10-02 21:24:45 +0100

    styling the stars

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import './index.css';
 //import App from './App';
-import Star from './Star';
+import StarBox from './StarBox';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Star maxRating={20}></Star>
-    <Star></Star>
+    <StarBox maxRating={10} className='test' defaultRating={1}></StarBox>
+    <StarBox size={15} color='red' className='test' />
+    
   </React.StrictMode>
 );
 
