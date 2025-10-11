0f2e80495cb2c3db32c3a93cd524e4d6640b0879
Marizet Pedro <marizet.contact@gmail.com>
2025-10-11 13:57:07 +0100

    error handling

--- src/App.js
+++ src/App.js
@@ -260,19 +260,39 @@ function WatchedMovie({movie}){
 
 const KEY = '53cbbe1f'
 
+function ErrorMessage({message}){
+  return <>
+    <p>{message}</p>
+  </>
+}
+
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false)
-  const query = 'naruto'
+  const [error, setError] = useState('')
+  const query = 'dsdgae'
 
   useEffect(function(){
     async function fetchMovies () {
-      setIsLoading(true)
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
-      const data = await res.json()
-      setMovies(data.Search)
-      setIsLoading(false)
+      try{
+          setIsLoading(true)
+          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
+
+          if (!res.ok) throw new Error('Something went wrong!');
+
+          const data = await res.json();
+
+          if (data.Response === 'False') throw new Error('Movie not found')
+          setMovies(data.Search)
+          
+      } catch(err){
+        console.error(err.message)
+        setError(err.message)
+      } finally{
+        setIsLoading(false)
+      }
+      
     }
 
     fetchMovies()
@@ -289,7 +309,9 @@ export default function App() {
       </Nav>
       <Main >
         <Box>
-          {isLoading? <Loader/> : <MovieList movies={movies}/>}
+          {isLoading && <Loader/>}
+          {!isLoading && !error && <MovieList movies={movies}/>}
+          {error && <ErrorMessage message={error}/>}
         </Box>
         <Box>
             <Summary watched={watched}/>
